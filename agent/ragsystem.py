3d6d6c80436cbc3a6b2d89b49f27577c36b61ba1
werayco <werayco@gmail.com>
2026-01-02 02:46:17 -0800

    added-rag-v2

--- agent/ragsystem.py
+++ agent/ragsystem.py
@@ -3,7 +3,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from docling.document_converter import DocumentConverter
 import warnings
 import os
 
